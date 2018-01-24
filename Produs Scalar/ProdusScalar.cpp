381dbe9b4217e99e4f1d46403c25f59615a62d89
Bogdan Andrisan <bobo1793@gmail.com>
2018-01-24 10:19:00 -0800

    wip prod scalar

--- Produs Scalar/ProdusScalar.cpp
+++ Produs Scalar/ProdusScalar.cpp
@@ -1,3 +1,9 @@
+/* 
+Se defineste produsul scalar a doi vectori A si B, de aceasi dimensiune n cu formula:
+PS = sum(i=1,n, A(i)*B(i)
+Sa se scrie algoritmul pentru calculul produsului scalar
+*/
+
 int main(int argc, char* argv[])
 {
 	
