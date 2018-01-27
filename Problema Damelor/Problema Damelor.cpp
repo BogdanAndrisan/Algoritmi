f83bc91cbf238a7abc44a38b74f940081ea9271f
Bogdan Andrisan <bobo1793@gmail.com>
2018-01-26 16:48:36 -0800

    Problema damelor wip

---
+++ Problema Damelor/Problema Damelor.cpp
@@ -0,0 +1,95 @@
+﻿/*
+Dame(n)
+	pentru i = 1, n
+		x(i) = 0{iniţial pe nici o linie nu avem poziţionată nici o regină}
+	sfârşit pentru
+	k = 1
+	cât timp k > 0
+		dacă k = n + 1 atunci
+			scrie( (x(i)), i = 1, n )
+			k = k − 1
+		altfel
+			x(k) = x(k) + 1
+			dacă x(k) ≤ n atunci
+				dacă DispunereCorecta(x, k)=adevarat atunci
+					k = k + 1
+				sfârşit dacă
+			altfel
+				x(k) = 0{ Nu avem nici o regină pe această linie} 
+				k = k − 1{ Ne întoarcem la regina de pe poziţia anterioară} 
+			sfârşit dacă{x(k) ≤ n}
+		sfârşit dacă{ k = n + 1}
+	sfârşit cât timp
+Return
+*/
+/*
+DispunereCorecta(x, k)
+	corect = adevarat
+	{ presupunem că nu avem nici un atac între regina k şi cele dispuse anterior}
+	pentru i = 1, k − 1
+		dacă x(k) = x(i) sau |x(k) − x(i)| = k − i atunci
+			{ sunt pe aceeaşi coloană sau pe aceeaşi diagonală}
+			corect = fals
+		sfârşit dacă
+	sfârşit pentru
+	DispunereCorecta = corect
+Return
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void scrie(int *x, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf_s(" %d ", x[i]);
+	}
+}
+
+bool DispunereCorecta(int *x, int k)
+{
+	bool corect = true;
+	for (int i = 0; i < k-1; i++)
+	{
+		if (x[k] == x[i] || x[k] - x[i] == k - i)
+		{
+			corect = false;
+		}
+	}
+	return corect;
+}
+
+int main()
+{
+	int n = 4;
+	int *x = (int*)malloc((n*n) * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		x[i] = 0;
+	}
+	int k = 1;
+	while (k > 0)
+	{
+		if (k = n + 1)
+		{
+			scrie(x,n);
+			k = k - 1;
+		}
+		else
+		{
+			x[k] = x[k] + 1;
+			if (x[k] <= n)
+			{
+				if (DispunereCorecta(x, k) == true)
+				{
+					k = k + 1;
+				}
+			}
+			else
+			{
+				x[k] = 0;
+				k = k - 1;
+			}
+		}
+	}
+}
